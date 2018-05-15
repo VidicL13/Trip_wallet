6a6c409fe2b96d1786e152f6fae5c434b13fbb83
Luka Vidic <luka94vidic@gmail.com>
2018-05-15 12:54:27 +0200

    Nastaviti bo potrebno head, drugače pa runserver že deluje. Naredi še ikono na zavihku.

--- Trip_wallet/webapp/urls.py
+++ Trip_wallet/webapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name = 'INDEKS')
+    path('',views.login, name = 'MAIN')
+    path('login/', views.login, name = 'INDEKS'),
+    path('signin/', views.signin, name = 'INDEKS')
 ]
--- Trip_wallet/webapp/views.py
+++ Trip_wallet/webapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, template_name='webapp/test.html')
+    return render(request, template_name='webapp/login.html')
+
+def signin(request):
+    return render(request, template_name='webapp/signin.html')
