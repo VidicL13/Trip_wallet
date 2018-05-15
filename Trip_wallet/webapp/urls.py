7a99f8f638b43c949efd3ecabafeaf73fc58810c
Luka Vidic <luka94vidic@gmail.com>
2018-05-15 23:39:49 +0200

    popravki v urls in login

--- Trip_wallet/webapp/urls.py
+++ Trip_wallet/webapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.login, name = 'MAIN')
+    path('',views.login, name = 'MAIN'),
     path('login/', views.login, name = 'INDEKS'),
     path('signin/', views.signin, name = 'INDEKS')
 ]
