7b1f5396525057a4c11f25941ddbd045ecbf3c88
Luka Vidic <luka94vidic@gmail.com>
2018-06-02 02:04:53 +0200

    vzpostavil sem neko osnovno bazo, ki pa jo bo zagotovo potrebno še spremeniti. Prav tako sem v bazo vnesel admina.

--- Trip_wallet/webapp/admin.py
+++ Trip_wallet/webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(PersonalInformation)
+admin.site.register(ExchangeRate)
+admin.site.register(Transaction)
+admin.site.register(TransactionInvolvment)
+admin.site.register(TransactionLabels)
+admin.site.register(TransactionItems)
+admin.site.register(Trip)
--- Trip_wallet/webapp/models.py
+++ Trip_wallet/webapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PersonalInformation(models.Model):
+    user = models.OneToOneField(User,related_name='user', on_delete=models.CASCADE)
+    country = models.CharField(max_length=250, blank=True)
+    address = models.CharField(max_length=250, blank=True)
+    birth_date = models.DateField(blank=True)
+    telephone_number = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(blank=True)
+    tax_number = models.CharField(max_length=20, blank=True)
+    creator = models.OneToOneField(User, related_name='creator', on_delete=models.SET_NULL, blank=True, null=True)
+    user_type = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.address
+
+class ExchangeRate(models.Model):
+    currancy = models.CharField(max_length=30, blank=True)
+    value = models.FloatField(blank=True)
+
+class Transaction(models.Model):
+    time_stamp = models.DateTimeField(default=timezone.now)
+    value = models.FloatField(blank=True)
+    user_payed = models.OneToOneField(User, related_name='user_payed', on_delete=models.PROTECT)
+    user_recieved = models.OneToOneField(User, related_name='user_recieved', on_delete=models.PROTECT, null=True, blank=True)
+    type = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, null=True)
+
+class TransactionInvolvment(models.Model):
+    transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
+    weight = models.FloatField(blank=False, null=False)
+
+class TransactionLabels(models.Model):
+    transaction = models.ManyToManyField(Transaction)
+    label = models.CharField(max_length=100)
+
+class TransactionItems(models.Model):
+    transaction = models.ManyToManyField(Transaction)
+    item = models.CharField(max_length=100)
+
+class Trip(models.Model):
+    name = models.CharField(max_length=250)
+    country = models.CharField(max_length=250, blank=True)
+    users = models.ManyToManyField(User, related_name='participants')
+    discription =  models.CharField(max_length=2500, blank=True)
